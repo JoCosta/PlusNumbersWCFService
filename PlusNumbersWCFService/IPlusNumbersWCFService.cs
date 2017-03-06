638d048ef0b86b14a8b7b035985aaf9d656d8135
Jolene <jolenecosta@gmail.com>
2017-03-06 16:21:20 -0500

    PlusNumbers WCF Service

---
+++ PlusNumbersWCFService/IPlusNumbersWCFService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PlusNumbersWCFService
+{
+    [ServiceContract]
+    public interface IPlusNumbersWCFService
+    {
+
+        [OperationContract]
+        int PlusNumbers(int a, int b);    
+    }
+  
+}
